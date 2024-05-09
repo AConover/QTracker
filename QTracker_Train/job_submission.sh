99da686cecde88bf51c5fb4bd41e2954cc9aae04
AConover <50592596+AConover@users.noreply.github.com>
2024-05-09 16:51:47 -0400

    Update job_submission.sh
    Added dump track finder and kinematics training.

--- QTracker_Train/job_submission.sh
+++ QTracker_Train/job_submission.sh
@@ -12,15 +12,21 @@ job3_id=$(sbatch --dependency=afterok:$job1_id Training_Jobscripts/jobscript_TFZ
 # Submit Target-Vertex Finding Job, start once the event filter is done.
 job4_id=$(sbatch --dependency=afterok:$job1_id Training_Jobscripts/jobscript_TFT.sh | awk '{print $4}')
 
+# Submit Dump-Vertex Finding Job, start once the event filter is done.
+job5_id=$(sbatch --dependency=afterok:$job1_id Training_Jobscripts/jobscript_TFD.sh | awk '{print $4}')
+
 # Submit the All-Vertex Kinematic and Vertex Reconstruction job once the All-Vertex Finder is done training.
-job5_id=$(sbatch --dependency=afterok:$job2_id Training_Jobscripts/jobscript_Kin_All.sh | awk '{print $4}')
+job6_id=$(sbatch --dependency=afterok:$job2_id Training_Jobscripts/jobscript_Kin_All.sh | awk '{print $4}')
 
 # Submit the Z-Vertex Kinematic and Vertex Reconstruction job once the Z-Vertex Finder is done training.
-job6_id=$(sbatch --dependency=afterok:$job3_id Training_Jobscripts/jobscript_Kin_Z.sh | awk '{print $4}')
+job7_id=$(sbatch --dependency=afterok:$job3_id Training_Jobscripts/jobscript_Kin_Z.sh | awk '{print $4}')
 
 # Submit the Target-Vertex Kinematic Reconstruction job once the Target-Vertex Finder is done training.
-job7_id=$(sbatch --dependency=afterok:$job4_id Training_Jobscripts/jobscript_Kin_Target.sh | awk '{print $4}')
+job8_id=$(sbatch --dependency=afterok:$job4_id Training_Jobscripts/jobscript_Kin_Target.sh | awk '{print $4}')
+
+# Submit the Dump-Vertex Kinematic Reconstruction job once the Target-Vertex Finder is done training.
+job8_id=$(sbatch --dependency=afterok:$job5_id Training_Jobscripts/jobscript_Kin_Dump.sh | awk '{print $4}')
 
 # Submit the target-dump filter training once everything else is done.
-sbatch --dependency=afterok:$job5_id:$job6_id:$job7_id Training_Jobscripts/jobscript_Target_Dump_Filter.sh
+sbatch --dependency=afterok:$job6_id:$job7_id:$job8_id:$job9_id Training_Jobscripts/jobscript_Target_Dump_Filter.sh
 
