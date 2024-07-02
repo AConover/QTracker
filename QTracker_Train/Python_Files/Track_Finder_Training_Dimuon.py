db97ae80bd6d3ed3ac4f721dcf646f2707efe621
AConover <50592596+AConover@users.noreply.github.com>
2024-07-02 17:02:20 -0400

    Update Track_Finder_Training_Dimuon.py

--- QTracker_Train/Python_Files/Track_Finder_Training_Dimuon.py
+++ QTracker_Train/Python_Files/Track_Finder_Training_Dimuon.py
@@ -98,10 +98,25 @@ while(n_train<1e7):
         val_predictions = probability_model.predict(valin, batch_size=225)
         train_mask = train_predictions[:, 3] > 0.75
         val_mask = val_predictions[:, 3] > 0.75
-        trainin = trainin[train_mask]
-        traintrack = traintrack[train_mask]
-        valin = valin[val_mask]
-        valtrack = valtrack[val_mask]
+    with tf.keras.backend.clear_session():
+        track_finder_pos = tf.keras.models.load_model('Networks/Track_Finder_Pos')
+        track_finder_neg = tf.keras.models.load_model('Networks/Track_Finder_Neg')
+        pos_predictions = (np.round(track_finder_pos.predict(valin, verbose=0) * max_ele)).astype(int)
+        neg_predictions = (np.round(track_finder_neg.predict(valin, verbose=0) * max_ele)).astype(int)
+        track_val = evaluate_finder(valin, valdrift, np.column_stack((pos_predictions,neg_predictions)))
+        results = calc_mismatches(track_val)
+        train_mask = train_mask & ((results[0::4] < 2) & (results[1::4] < 2) & (results[2::4] < 3) & (results[3::4] < 3)).all(axis=0)
+    
+        pos_predictions = (np.round(track_finder_pos.predict(trainin, verbose=0) * max_ele)).astype(int)
+        neg_predictions = (np.round(track_finder_neg.predict(trainin, verbose=0) * max_ele)).astype(int)
+        track_train = evaluate_finder(trainin, traindrift, np.column_stack((pos_predictions,neg_predictions)))
+        results = calc_mismatches(track_train)
+        val_mask = val_mask & ((results[0::4] < 2) & (results[1::4] < 2) & (results[2::4] < 3) & (results[3::4] < 3)).all(axis=0)
+    
+    trainin = trainin[train_mask]
+    traintrack = traintrack[train_mask]
+    valin = valin[val_mask]
+    valtrack = valtrack[val_mask]
     
     n_train+=len(trainin)
         
