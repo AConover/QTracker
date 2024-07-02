034cc4798528be03af01bc255cf11c002bdb1ef5
AConover <50592596+AConover@users.noreply.github.com>
2024-07-02 14:19:30 -0400

    Update Event_Filter_Training.py

--- QTracker_Train/Python_Files/Event_Filter_Training.py
+++ QTracker_Train/Python_Files/Event_Filter_Training.py
@@ -6,284 +6,73 @@ import random
 import tensorflow as tf
 import gc
 
-
 def read_root_file(root_file):
-    print("Reading ROOT files...")
-    #Reads information from MC.
-    targettree = uproot.open(root_file+':QA_ana')
-    targetevents=len(targettree['n_tracks'].array(library='np'))
-    D0U_ele = targettree['D0U_ele'].array(library='np')
-    D0Up_ele = targettree['D0Up_ele'].array(library='np')
-    D0X_ele = targettree['D0X_ele'].array(library='np')
-    D0Xp_ele = targettree['D0Xp_ele'].array(library='np')
-    D0V_ele = targettree['D0V_ele'].array(library='np')
-    D0Vp_ele = targettree['D0Vp_ele'].array(library='np')
+    targettree = uproot.open(root_file + ':QA_ana')
+    targetevents = len(targettree['n_tracks'].array(library='np'))
 
-    D2U_ele = targettree['D2U_ele'].array(library='np')
-    D2Up_ele = targettree['D2Up_ele'].array(library='np')
-    D2X_ele = targettree['D2X_ele'].array(library='np')
-    D2Xp_ele = targettree['D2Xp_ele'].array(library='np')
-    D2V_ele = targettree['D2V_ele'].array(library='np')
-    D2Vp_ele = targettree['D2Vp_ele'].array(library='np')
+    detector_data = read_detector_data(targettree)
 
-    D3pU_ele = targettree['D3pU_ele'].array(library='np')
-    D3pUp_ele = targettree['D3pUp_ele'].array(library='np')
-    D3pX_ele = targettree['D3pX_ele'].array(library='np')
-    D3pXp_ele = targettree['D3pXp_ele'].array(library='np')
-    D3pV_ele = targettree['D3pV_ele'].array(library='np')
-    D3pVp_ele = targettree['D3pVp_ele'].array(library='np')
-
-    D3mU_ele = targettree['D3mU_ele'].array(library='np')
-    D3mUp_ele = targettree['D3mUp_ele'].array(library='np')
-    D3mX_ele = targettree['D3mX_ele'].array(library='np')
-    D3mXp_ele = targettree['D3mXp_ele'].array(library='np')
-    D3mV_ele = targettree['D3mV_ele'].array(library='np')
-    D3mVp_ele = targettree['D3mVp_ele'].array(library='np')
-
-    D0U_drift = targettree['D0U_drift'].array(library='np')
-    D0Up_drift = targettree['D0Up_drift'].array(library='np')
-    D0X_drift = targettree['D0X_drift'].array(library='np')
-    D0Xp_drift = targettree['D0Xp_drift'].array(library='np')
-    D0V_drift = targettree['D0V_drift'].array(library='np')
-    D0Vp_drift = targettree['D0Vp_drift'].array(library='np')
-
-    D2U_drift = targettree['D2U_drift'].array(library='np')
-    D2Up_drift = targettree['D2Up_drift'].array(library='np')
-    D2X_drift = targettree['D2X_drift'].array(library='np')
-    D2Xp_drift = targettree['D2Xp_drift'].array(library='np')
-    D2V_drift = targettree['D2V_drift'].array(library='np')
-    D2Vp_drift = targettree['D2Vp_drift'].array(library='np')
-
-    D3pU_drift = targettree['D3pU_drift'].array(library='np')
-    D3pUp_drift = targettree['D3pUp_drift'].array(library='np')
-    D3pX_drift = targettree['D3pX_drift'].array(library='np')
-    D3pXp_drift = targettree['D3pXp_drift'].array(library='np')
-    D3pV_drift = targettree['D3pV_drift'].array(library='np')
-    D3pVp_drift = targettree['D3pVp_drift'].array(library='np')
-
-    D3mU_drift = targettree['D3mU_drift'].array(library='np')
-    D3mUp_drift = targettree['D3mUp_drift'].array(library='np')
-    D3mX_drift = targettree['D3mX_drift'].array(library='np')
-    D3mXp_drift = targettree['D3mXp_drift'].array(library='np')
-    D3mV_drift = targettree['D3mV_drift'].array(library='np')
-    D3mVp_drift = targettree['D3mVp_drift'].array(library='np')
-
-    H1B_ele = targettree['H1B_ele'].array(library='np')
-    H1T_ele = targettree['H1T_ele'].array(library='np')
-    H1L_ele = targettree['H1L_ele'].array(library='np')
-    H1R_ele = targettree['H1R_ele'].array(library='np')
-
-    H2L_ele = targettree['H2L_ele'].array(library='np')
-    H2R_ele = targettree['H2R_ele'].array(library='np')
-    H2B_ele = targettree['H2B_ele'].array(library='np')
-    H2T_ele = targettree['H2T_ele'].array(library='np')
-
-    H3B_ele = targettree['H3B_ele'].array(library='np')
-    H3T_ele = targettree['H3T_ele'].array(library='np')
-
-    H4Y1L_ele = targettree['H4Y1L_ele'].array(library='np')
-    H4Y1R_ele = targettree['H4Y1R_ele'].array(library='np')
-    H4Y2L_ele = targettree['H4Y2L_ele'].array(library='np')
-    H4Y2R_ele = targettree['H4Y2R_ele'].array(library='np')
-    H4B_ele = targettree['H4B_ele'].array(library='np')
-    H4T_ele = targettree['H4T_ele'].array(library='np')
-
-    P1Y1_ele = targettree['P1Y1_ele'].array(library='np')
-    P1Y2_ele = targettree['P1Y2_ele'].array(library='np')
-    P1X1_ele = targettree['P1X1_ele'].array(library='np')
-    P1X2_ele = targettree['P1X2_ele'].array(library='np')
-
-    P2X1_ele = targettree['P2X1_ele'].array(library='np')
-    P2X2_ele = targettree['P2X2_ele'].array(library='np')
-    P2Y1_ele = targettree['P2Y1_ele'].array(library='np')
-    P2Y2_ele = targettree['P2Y2_ele'].array(library='np')
-
-    gpx = targettree['gpx'].array(library='np')
-    gpy = targettree['gpy'].array(library='np')
-    gpz = targettree['gpz'].array(library='np')
-    gvx = targettree['gvx'].array(library='np')
-    gvy = targettree['gvy'].array(library='np')
-    gvz = targettree['gvz'].array(library='np')
-
-    pid = targettree['pid'].array(library='np')
-
-    print('Done')
-
-    # Initialize arrays for storing event data
-    pos_events=np.zeros((targetevents,54))
-    pos_drift = np.zeros((targetevents,30))
-    pos_kinematics = np.zeros((targetevents,6))
-    neg_events=np.zeros((targetevents,54))
-    neg_drift = np.zeros((targetevents,30))
-    neg_kinematics = np.zeros((targetevents,6))
-    print("Reading target events...")
-    for j in range(targetevents):
-        first=pid[j][0]
-        if(first>0):
-            pos=0
-            neg=1
-        else:
-            pos=1
-            neg=0
-        pos_kinematics[j][0] = gpx[j][pos]
-        pos_kinematics[j][1] = gpy[j][pos]
-        pos_kinematics[j][2] = gpz[j][pos]
-        pos_kinematics[j][3] = gvx[j][pos]
-        pos_kinematics[j][4] = gvy[j][pos]
-        pos_kinematics[j][5] = gvz[j][pos]
-        neg_kinematics[j][0] = gpx[j][neg]
-        neg_kinematics[j][1] = gpy[j][neg]
-        neg_kinematics[j][2] = gpz[j][neg]
-        neg_kinematics[j][3] = gvx[j][neg]
-        neg_kinematics[j][4] = gvy[j][neg]
-        neg_kinematics[j][5] = gvz[j][neg]
-        pos_events[j][0]=D0U_ele[j][pos]
-        neg_events[j][0]=D0U_ele[j][neg]
-        pos_events[j][1]=D0Up_ele[j][pos]
-        neg_events[j][1]=D0Up_ele[j][neg]
-        pos_events[j][2]=D0X_ele[j][pos]
-        neg_events[j][2]=D0X_ele[j][neg]        
-        pos_events[j][3]=D0Xp_ele[j][pos]
-        neg_events[j][3]=D0Xp_ele[j][neg]
-        pos_events[j][4]=D0V_ele[j][pos]
-        neg_events[j][4]=D0V_ele[j][neg]
-        pos_events[j][5]=D0Vp_ele[j][pos]
-        neg_events[j][5]=D0Vp_ele[j][neg]
-        pos_events[j][16]=D2U_ele[j][pos]
-        neg_events[j][16]=D2U_ele[j][neg]
-        pos_events[j][17]=D2Up_ele[j][pos]
-        neg_events[j][17]=D2Up_ele[j][neg]
-        pos_events[j][15]=D2X_ele[j][pos]
-        neg_events[j][15]=D2X_ele[j][neg]
-        pos_events[j][14]=D2Xp_ele[j][pos]
-        neg_events[j][14]=D2Xp_ele[j][neg]
-        pos_events[j][12]=D2V_ele[j][pos]
-        neg_events[j][12]=D2V_ele[j][neg]
-        pos_events[j][13]=D2Vp_ele[j][pos]
-        neg_events[j][13]=D2Vp_ele[j][neg]
-        pos_events[j][23]=D3pU_ele[j][pos]
-        neg_events[j][23]=D3pU_ele[j][neg]
-        pos_events[j][22]=D3pUp_ele[j][pos]
-        neg_events[j][22]=D3pUp_ele[j][neg]
-        pos_events[j][21]=D3pX_ele[j][pos]
-        neg_events[j][21]=D3pX_ele[j][neg]
-        pos_events[j][20]=D3pXp_ele[j][pos]
-        neg_events[j][20]=D3pXp_ele[j][neg]
-        pos_events[j][19]=D3pV_ele[j][pos]
-        neg_events[j][19]=D3pV_ele[j][neg]
-        pos_events[j][18]=D3pVp_ele[j][pos]
-        neg_events[j][18]=D3pVp_ele[j][neg]
-        pos_events[j][29]=D3mU_ele[j][pos]
-        neg_events[j][29]=D3mU_ele[j][neg]
-        pos_events[j][28]=D3mUp_ele[j][pos]
-        neg_events[j][28]=D3mUp_ele[j][neg]
-        pos_events[j][27]=D3mX_ele[j][pos]
-        neg_events[j][27]=D3mX_ele[j][neg]
-        pos_events[j][26]=D3mXp_ele[j][pos]
-        neg_events[j][26]=D3mXp_ele[j][neg]
-        pos_events[j][25]=D3mV_ele[j][pos]
-        neg_events[j][25]=D3mV_ele[j][neg]
-        pos_events[j][24]=D3mVp_ele[j][pos]
-        neg_events[j][24]=D3mVp_ele[j][neg]
-        pos_events[j][30]=H1B_ele[j][pos]
-        neg_events[j][30]=H1B_ele[j][neg]
-        pos_events[j][31]=H1T_ele[j][pos]
-        neg_events[j][31]=H1T_ele[j][neg]
-        pos_events[j][32]=H1L_ele[j][pos]
-        neg_events[j][32]=H1L_ele[j][neg]
-        pos_events[j][33]=H1R_ele[j][pos]
-        neg_events[j][33]=H1R_ele[j][neg]
-        pos_events[j][34]=H2L_ele[j][pos]
-        neg_events[j][34]=H2L_ele[j][neg]
-        pos_events[j][35]=H2R_ele[j][pos]
-        neg_events[j][35]=H2R_ele[j][neg]
-        pos_events[j][36]=H2T_ele[j][pos]
-        neg_events[j][36]=H2T_ele[j][neg]
-        pos_events[j][37]=H2B_ele[j][pos]
-        neg_events[j][37]=H2B_ele[j][neg]
-        pos_events[j][38]=H3B_ele[j][pos]
-        neg_events[j][38]=H3B_ele[j][neg]
-        pos_events[j][39]=H3T_ele[j][pos]
-        neg_events[j][39]=H3T_ele[j][neg]
-        pos_events[j][40]=H4Y1L_ele[j][pos]
-        neg_events[j][40]=H4Y1L_ele[j][neg]
-        pos_events[j][41]=H4Y1R_ele[j][pos]
-        neg_events[j][41]=H4Y1R_ele[j][neg]
-        pos_events[j][42]=H4Y2L_ele[j][pos]
-        neg_events[j][42]=H4Y2L_ele[j][neg]
-        pos_events[j][43]=H4Y2R_ele[j][pos]
-        neg_events[j][43]=H4Y2R_ele[j][neg]
-        pos_events[j][44]=H4B_ele[j][pos]
-        neg_events[j][44]=H4B_ele[j][neg]
-        pos_events[j][45]=H4T_ele[j][pos]
-        neg_events[j][45]=H4T_ele[j][neg]
-        pos_events[j][46]=P1Y1_ele[j][pos]
-        neg_events[j][46]=P1Y1_ele[j][neg]
-        pos_events[j][47]=P1Y2_ele[j][pos]
-        neg_events[j][47]=P1Y2_ele[j][neg]
-        pos_events[j][48]=P1X1_ele[j][pos]
-        neg_events[j][48]=P1X1_ele[j][neg]
-        pos_events[j][49]=P1X2_ele[j][pos]
-        neg_events[j][49]=P1X2_ele[j][neg]
-        pos_events[j][50]=P2X1_ele[j][pos]
-        neg_events[j][50]=P2X1_ele[j][neg]
-        pos_events[j][51]=P2X2_ele[j][pos]
-        neg_events[j][51]=P2X2_ele[j][neg]
-        pos_events[j][52]=P2Y1_ele[j][pos]
-        neg_events[j][52]=P2Y1_ele[j][neg]
-        pos_events[j][53]=P2Y2_ele[j][pos]
-        neg_events[j][53]=P2Y2_ele[j][neg]
-        pos_drift[j][0]=D0U_drift[j][pos]
-        neg_drift[j][0]=D0U_drift[j][neg]
-        pos_drift[j][1]=D0Up_drift[j][pos]
-        neg_drift[j][1]=D0Up_drift[j][neg]
-        pos_drift[j][2]=D0X_drift[j][pos]
-        neg_drift[j][2]=D0X_drift[j][neg]        
-        pos_drift[j][3]=D0Xp_drift[j][pos]
-        neg_drift[j][3]=D0Xp_drift[j][neg]
-        pos_drift[j][4]=D0V_drift[j][pos]
-        neg_drift[j][4]=D0V_drift[j][neg]
-        pos_drift[j][5]=D0Vp_drift[j][pos]
-        neg_drift[j][5]=D0Vp_drift[j][neg]
-        pos_drift[j][16]=D2U_drift[j][pos]
-        neg_drift[j][16]=D2U_drift[j][neg]
-        pos_drift[j][17]=D2Up_drift[j][pos]
-        neg_drift[j][17]=D2Up_drift[j][neg]
-        pos_drift[j][15]=D2X_drift[j][pos]
-        neg_drift[j][15]=D2X_drift[j][neg]
-        pos_drift[j][14]=D2Xp_drift[j][pos]
-        neg_drift[j][14]=D2Xp_drift[j][neg]
-        pos_drift[j][12]=D2V_drift[j][pos]
-        neg_drift[j][12]=D2V_drift[j][neg]
-        pos_drift[j][13]=D2Vp_drift[j][pos]
-        neg_drift[j][13]=D2Vp_drift[j][neg]
-        pos_drift[j][23]=D3pU_drift[j][pos]
-        neg_drift[j][23]=D3pU_drift[j][neg]
-        pos_drift[j][22]=D3pUp_drift[j][pos]
-        neg_drift[j][22]=D3pUp_drift[j][neg]
-        pos_drift[j][21]=D3pX_drift[j][pos]
-        neg_drift[j][21]=D3pX_drift[j][neg]
-        pos_drift[j][20]=D3pXp_drift[j][pos]
-        neg_drift[j][20]=D3pXp_drift[j][neg]
-        pos_drift[j][19]=D3pV_drift[j][pos]
-        neg_drift[j][19]=D3pV_drift[j][neg]
-        pos_drift[j][18]=D3pVp_drift[j][pos]
-        neg_drift[j][18]=D3pVp_drift[j][neg]
-        pos_drift[j][29]=D3mU_drift[j][pos]
-        neg_drift[j][29]=D3mU_drift[j][neg]
-        pos_drift[j][28]=D3mUp_drift[j][pos]
-        neg_drift[j][28]=D3mUp_drift[j][neg]
-        pos_drift[j][27]=D3mX_drift[j][pos]
-        neg_drift[j][27]=D3mX_drift[j][neg]
-        pos_drift[j][26]=D3mXp_drift[j][pos]
-        neg_drift[j][26]=D3mXp_drift[j][neg]
-        pos_drift[j][25]=D3mV_drift[j][pos]
-        neg_drift[j][25]=D3mV_drift[j][neg]
-        pos_drift[j][24]=D3mVp_drift[j][pos]
-        neg_drift[j][24]=D3mVp_drift[j][neg]
-    print("Done")
-
+    pos_events = np.zeros((targetevents, 54))
+    pos_drift = np.zeros((targetevents, 30))
+    pos_kinematics = np.zeros((targetevents, 6))
+    neg_events = np.zeros((targetevents, 54))
+    neg_drift = np.zeros((targetevents, 30))
+    neg_kinematics = np.zeros((targetevents, 6))
+    
+    process_target_events(targetevents, detector_data, pos_events, pos_drift, pos_kinematics, neg_events, neg_drift, neg_kinematics)
+    
     return pos_events, pos_drift, pos_kinematics, neg_events, neg_drift, neg_kinematics
+
+def read_detector_data(targettree):
+    detector_data = {}
+    for key in ['D0U_ele', 'D0Up_ele', 'D0X_ele', 'D0Xp_ele', 'D0V_ele', 'D0Vp_ele', 
+                'D2U_ele', 'D2Up_ele', 'D2X_ele', 'D2Xp_ele', 'D2V_ele', 'D2Vp_ele', 
+                'D3pU_ele', 'D3pUp_ele', 'D3pX_ele', 'D3pXp_ele', 'D3pV_ele', 'D3pVp_ele', 
+                'D3mU_ele', 'D3mUp_ele', 'D3mX_ele', 'D3mXp_ele', 'D3mV_ele', 'D3mVp_ele', 
+                'H1B_ele', 'H1T_ele', 'H1L_ele', 'H1R_ele', 'H2L_ele', 'H2R_ele', 'H2B_ele', 'H2T_ele', 
+                'H3B_ele', 'H3T_ele', 'H4Y1L_ele', 'H4Y1R_ele', 'H4Y2L_ele', 'H4Y2R_ele', 'H4B_ele', 'H4T_ele', 
+                'P1Y1_ele', 'P1Y2_ele', 'P1X1_ele', 'P1X2_ele', 'P2X1_ele', 'P2X2_ele', 'P2Y1_ele', 'P2Y2_ele', 
+                'gpx', 'gpy', 'gpz', 'gvx', 'gvy', 'gvz', 'pid']:
+        detector_data[key] = targettree[key].array(library='np')
+    return detector_data
+
+def process_target_events(targetevents, detector_data, pos_events, pos_drift, pos_kinematics, neg_events, neg_drift, neg_kinematics):
+    pid = detector_data['pid']
+    gpx = detector_data['gpx']
+    gpy = detector_data['gpy']
+    gpz = detector_data['gpz']
+    gvx = detector_data['gvx']
+    gvy = detector_data['gvy']
+    gvz = detector_data['gvz']
+    
+    for j in range(targetevents):
+        first = pid[j][0]
+        pos, neg = (0, 1) if first > 0 else (1, 0)
+        
+        fill_kinematics(gpx, gpy, gpz, gvx, gvy, gvz, pos_kinematics, neg_kinematics, j, pos, neg)
+        fill_events_and_drift(detector_data, pos_events, pos_drift, neg_events, neg_drift, j, pos, neg)
+
+def fill_kinematics(gpx, gpy, gpz, gvx, gvy, gvz, pos_kinematics, neg_kinematics, j, pos, neg):
+    pos_kinematics[j][0] = gpx[j][pos]
+    pos_kinematics[j][1] = gpy[j][pos]
+    pos_kinematics[j][2] = gpz[j][pos]
+    pos_kinematics[j][3] = gvx[j][pos]
+    pos_kinematics[j][4] = gvy[j][pos]
+    pos_kinematics[j][5] = gvz[j][pos]
+    neg_kinematics[j][0] = gpx[j][neg]
+    neg_kinematics[j][1] = gpy[j][neg]
+    neg_kinematics[j][2] = gpz[j][neg]
+    neg_kinematics[j][3] = gvx[j][neg]
+    neg_kinematics[j][4] = gvy[j][neg]
+    neg_kinematics[j][5] = gvz[j][neg]
+
+def fill_events_and_drift(detector_data, pos_events, pos_drift, neg_events, neg_drift, j, pos, neg):
+    for k in range(54):
+        pos_events[j][k] = detector_data[f'key_{k}_ele'][j][pos]
+        neg_events[j][k] = detector_data[f'key_{k}_ele'][j][neg]
+    for k in range(30):
+        pos_drift[j][k] = detector_data[f'key_{k}_drift'][j][pos]
+        neg_drift[j][k] = detector_data[f'key_{k}_drift'][j][neg]
 
 
 # Reading training and validation data from ROOT files
