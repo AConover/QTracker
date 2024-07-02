21375df6bb078842f8de6b14fbdb29f385406044
AConover <50592596+AConover@users.noreply.github.com>
2024-07-02 14:05:27 -0400

    Update Event_Filter_Training.py

--- QTracker_Train/Python_Files/Event_Filter_Training.py
+++ QTracker_Train/Python_Files/Event_Filter_Training.py
@@ -1,13 +1,8 @@
 import os
 import numpy as np
 import uproot
-import numba
-from numba import cuda
 from numba import njit, prange
-import matplotlib.pyplot as plt
 import random
-from tensorflow.keras import datasets, layers, models, losses
-from tensorflow import keras
 import tensorflow as tf
 import gc
 
@@ -238,11 +233,10 @@ def read_root_file(root_file):
         neg_events[j][52]=P2Y1_ele[j][neg]
         pos_events[j][53]=P2Y2_ele[j][pos]
         neg_events[j][53]=P2Y2_ele[j][neg]
-        pos_drift[j][pos]=D0U_drift[j][pos]
-        pos_drift[j][pos]=D0U_drift[j][pos]
-        neg_drift[j][pos]=D0U_drift[j][neg]
-        pos_drift[j][neg]=D0Up_drift[j][pos]
-        neg_drift[j][neg]=D0Up_drift[j][neg]
+        pos_drift[j][0]=D0U_drift[j][pos]
+        neg_drift[j][0]=D0U_drift[j][neg]
+        pos_drift[j][1]=D0Up_drift[j][pos]
+        neg_drift[j][1]=D0Up_drift[j][neg]
         pos_drift[j][2]=D0X_drift[j][pos]
         neg_drift[j][2]=D0X_drift[j][neg]        
         pos_drift[j][3]=D0Xp_drift[j][pos]
@@ -291,7 +285,6 @@ def read_root_file(root_file):
 
     return pos_events, pos_drift, pos_kinematics, neg_events, neg_drift, neg_kinematics
 
-
 pos_events, pos_drift, pos_kinematics, neg_events, neg_drift, neg_kinematics = read_root_file('Root_Files/Target_Train_QA_v2.root')
 pos_events_val, pos_drift_val, pos_kinematics_val, neg_events_val, neg_drift_val, neg_kinematics_val = read_root_file('Root_Files/Target_Val_QA_v2.root')
 
@@ -303,7 +296,6 @@ def clean(events):
                 events[j][i]=0
     return events
 
-
 pos_events=clean(pos_events).astype(int)
 neg_events=clean(neg_events).astype(int)
 pos_events_val=clean(pos_events_val).astype(int)
@@ -312,161 +304,26 @@ neg_events_val=clean(neg_events_val).astype(int)
 @njit(parallel=True)
 def track_injection(hits,pos_e,neg_e):
     #Start generating the events
-    category=np.zeros((len(hits),1))
+    category=np.zeros((len(hits)))
     track=np.zeros((len(hits),108))
-    trackreal=np.zeros((len(hits),68))
     for z in prange(len(hits)):
-        m = random.randrange(0,12)#For track finding, set this to m = 7 (only generates dimuons)
-        if(m==2) or (m==3):#Single Muon
-            j=random.randrange(len(pos_e)) 
-            l=random.randrange(0,2)
-            if(l==0):
-                for k in range(54):
-                    if(pos_e[j][k]>0):
-                        if(random.random()<0.94) or (k>29):
-                            hits[z][k][int(pos_e[j][k]-1)]=1
-            if(l==1):
-                for k in range(54):
-                    if(neg_e[j][k]>0):
-                        if(random.random()<0.94) or (k>29):
-                            hits[z][k][int(neg_e[j][k]-1)]=1
-            category[z][0]=1
-        if(m==4) or (m==5):#Two Muons of the Same Sign
-            j=random.randrange(len(pos_e))
-            l=random.randrange(0,2)
-            if(l==0):
-                for k in range(54):
-                    if(pos_e[j][k]>0):
-                        if(random.random()<0.94) or (k>29):
-                            hits[z][k][int(pos_e[j][k]-1)]=1
-                j=random.randrange(len(pos_e))
-                for k in range(54):
-                    if(pos_e[j][k]>0):
-                        if(random.random()<0.94) or (k>29):
-                            hits[z][k][int(pos_e[j][k]-1)]=1
-            if(l==1):
-                for k in range(54):
-                    if(neg_e[j][k]>0):
-                        if(random.random()<0.94) or (k>29):
-                            hits[z][k][int(neg_e[j][k]-1)]=1
-                j=random.randrange(len(pos_e))
-                for k in range(54):
-                    if(neg_e[j][k]>0):
-                        if(random.random()<0.94) or (k>29):
-                            hits[z][k][int(neg_e[j][k]-1)]=1
-            category[z][0]=2
-        if(m==6):#Two Muons of opposite signs
-            j=random.randrange(len(pos_e))
-            j2=random.randrange(len(neg_e))
-            for k in range(54):
-                if(pos_e[j][k]>0):
-                    if(random.random()<0.94) or (k>29):
-                        hits[z][k][int(pos_e[j][k]-1)]=1
-                if(neg_e[j2][k]>0):
-                    if(random.random()<0.94) or (k>29):
-                        hits[z][k][int(neg_e[j2][k]-1)]=1
-            category[z][0]=3
-        if(m==7):#Dimuon pair
-            j=random.randrange(len(pos_e))
-            j2=j#random.randrange(len(neg_e))
-            for k in range(54):
-                if(pos_e[j][k]>0):
-                    if(random.random()<0.94) or (k>29):
-                        hits[z][k][int(pos_e[j][k]-1)]=1
-                    track[z][k]=pos_e[j][k]
-                if(neg_e[j2][k]>0):
-                    if(random.random()<0.94) or (k>29):
-                        hits[z][k][int(neg_e[j2][k]-1)]=1
-                    track[z][k+54]=neg_e[j2][k]
-            category[z][0]=3
-            
-        if(m==8) or (m==9):#Three muons of the same sign
-            j=random.randrange(len(pos_e)) #Select random event number
-            l=random.randrange(0,2)
-            if(l==0):
-                for k in range(54):
-                    if(pos_e[j][k]>0):
-                        if(random.random()<0.94) or (k>29):
-                            hits[z][k][int(pos_e[j][k]-1)]=1
-                j=random.randrange(len(pos_e))
-                for k in range(54):
-                    if(pos_e[j][k]>0):
-                        if(random.random()<0.94) or (k>29):
-                            hits[z][k][int(pos_e[j][k]-1)]=1
-                j=random.randrange(len(pos_e))
-                for k in range(54):
-                    if(pos_e[j][k]>0):
-                        if(random.random()<0.94) or (k>29):
-                            hits[z][k][int(pos_e[j][k]-1)]=1
-            if(l==1):
-                for k in range(54):
-                    if(neg_e[j][k]>0):
-                        if(random.random()<0.94) or (k>29):
-                            hits[z][k][int(neg_e[j][k]-1)]=1
-                j=random.randrange(len(pos_e))
-                for k in range(54):
-                    if(neg_e[j][k]>0):
-                        if(random.random()<0.94) or (k>29):
-                            hits[z][k][int(neg_e[j][k]-1)]=1
-                j=random.randrange(len(pos_e))
-                for k in range(54):
-                    if(neg_e[j][k]>0):
-                        if(random.random()<0.94) or (k>29):
-                            hits[z][k][int(neg_e[j][k]-1)]=1
-            category[z][0]=4
-            
-        if(m==10):#Three muons, one of the opposite sign of the other two.
-            j=random.randrange(len(pos_e))
-            j2=random.randrange(len(neg_e))
-            for k in range(54):
-                if(pos_e[j][k]>0):
-                    if(random.random()<0.94) or (k>29):
-                        hits[z][k][int(pos_e[j][k]-1)]=1
-                if(neg_e[j2][k]>0):
-                    if(random.random()<0.94) or (k>29):
-                        hits[z][k][int(neg_e[j2][k]-1)]=1
-            j=random.randrange(len(pos_e)) #Select random event number
-            l=random.randrange(0,2)
-            if(l==0):
-                for k in range(54):
-                    if(pos_e[j][k]>0):
-                        if(random.random()<0.94) or (k>29):
-                            hits[z][k][int(pos_e[j][k]-1)]=1
-            if(l==1):
-                for k in range(54):
-                    if(neg_e[j][k]>0):
-                        if(random.random()<0.94) or (k>29):
-                            hits[z][k][int(neg_e[j][k]-1)]=1
-            category[z][0]=5
-        if(m==11): #Dimuon pair and random extra muon
-            j=random.randrange(len(pos_e))
-            for k in range(54):
-                if(pos_e[j][k]>0):
-                    if(random.random()<0.94) or (k>29):
-                        hits[z][k][int(pos_e[j][k]-1)]=1
-                if(neg_e[j][k]>0):
-                    if(random.random()<0.94) or (k>29):
-                        hits[z][k][int(neg_e[j][k]-1)]=1
-            j=random.randrange(len(pos_e)) #Select random event number
-            l=random.randrange(0,2)
-            if(l==0):
-                for k in range(54):
-                    if(pos_e[j][k]>0):
-                        if(random.random()<0.94) or (k>29):
-                            hits[z][k][int(pos_e[j][k]-1)]=1
-            if(l==1):
-                for k in range(54):
-                    if(neg_e[j][k]>0):
-                        if(random.random()<0.94) or (k>29):
-                            hits[z][k][int(neg_e[j][k]-1)]=1
-            category[z][0]=5
-    return hits,category,trackreal
+        m = random.randrange(0,2)
+        j=random.randrange(len(pos_e))
+        j2=j#random.randrange(len(neg_e))
+        for k in range(54):
+            if(pos_e[j][k]>0):
+                if(random.random()<m*0.94) or ((k>29)&(k<45)):
+                    hits[z][k][int(pos_e[j][k]-1)]=1
+                track[z][k]=pos_e[j][k]
+            if(neg_e[j2][k]>0):
+                if(random.random()<m*0.94) or ((k>29)&(k<45)):
+                    hits[z][k][int(neg_e[j2][k]-1)]=1
+        category[z]=m        
+
+    return hits,category
 
 
-# In[7]:
-
-
-@numba.jit(nopython=True)
+@njit()
 def hit_matrix(detectorid,elementid,hits,station): #Convert into hit matrices
     for j in range (len(detectorid)):
         rand = random.random()
@@ -489,29 +346,6 @@ def hit_matrix(detectorid,elementid,hits,station): #Convert into hit matrices
                 hits[int(detectorid[j])-1][int(elementid[j]-1)]=1
         if(rand<0.25):
             if ((detectorid[j]>40) and (detectorid[j]<47)): 
-                hits[int(detectorid[j])-1][int(elementid[j]-1)]=1
-    return hits
-
-
-@numba.jit(nopython=True)
-def hit_matrix_mc(detectorid,elementid,hits,station): #Convert into hit matrices
-    for j in prange (len(detectorid)):
-        #St 1
-        if(station==1):
-            if ((detectorid[j]<7) or (detectorid[j]>30)) and (detectorid[j]<35):
-                hits[int(detectorid[j])-1][int(elementid[j]-1)]=1
-        #St 2
-        elif(station==2):
-            if (detectorid[j]>12 and (detectorid[j]<19)) or ((detectorid[j]>34) and (detectorid[j]<39)):
-                if((detectorid[j]<15) and (rand<0.76)) or ((detectorid[j]>14) and (rand<0.86)) or (detectorid[j]==17):
-                    hits[int(detectorid[j])-1][int(elementid[j]-1)]=1
-        #St 3
-        elif(station==3):
-            if (detectorid[j]>18 and (detectorid[j]<31)) or ((detectorid[j]>38) and (detectorid[j]<47)):
-                hits[int(detectorid[j])-1][int(elementid[j]-1)]=1
-        #St 4
-        elif(station==4):
-            if ((detectorid[j]>40) and (detectorid[j]<55)):
                 hits[int(detectorid[j])-1][int(elementid[j]-1)]=1
     return hits
 
@@ -541,85 +375,26 @@ def generate_e906(n_events, tvt):
     
     #Place the full tracks that are reconstructable
     if(tvt=="Train"):
-        hits,category,track=track_injection(hits,pos_events,neg_events)    
+        hits,category=track_injection(hits,pos_events,neg_events)    
     if(tvt=="Val"):
-        hits,category,track=track_injection(hits,pos_events_val,neg_events_val)    
-    return hits.astype(bool), category.astype(int), track.astype(int)
-
-
-
-def evaluate(testin,testsignals,modelname):    
-    # Use the trained model to predict the particle ID for each
-    model = keras.models.load_model('Networks/'+modelname)
-    probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
-    predictions = probability_model.predict(testin,verbose=0)
-    testpred=np.zeros(len(predictions))
-    for i in range(len(testpred)):
-        if predictions[i][1]>0.75:testpred[i]=1
-        if predictions[i][2]>0.75:testpred[i]=2
-        if predictions[i][3]>0.75:testpred[i]=3
-        if predictions[i][4]>0.75:testpred[i]=4
-        if predictions[i][5]>0.75:testpred[i]=5
-    #testpred = np.argmax(predictions, axis=1)
-
-    # Create arrays for true/false positives/negatives
-    tp = np.zeros(6)
-    tn = np.zeros(6)
-    fp = np.zeros(6)
-    fn = np.zeros(6)
-
-    # Loop through the predictions and true signals to calculate true/false positives/negatives
-    for i in range(len(testpred)):
-        pred = testpred[i]
-        signal = testsignals[i]
-        for j in range(6):
-            if pred == j:
-                if signal == j:
-                    tp[j] += 1
-                else:
-                    fp[j] += 1
-                for k in range(6):
-                    if k != j and signal != k:
-                        tn[k] += 1
-                    elif k != j and signal == k:
-                        fn[k] += 1
-
-    # Calculate precision and recall for each category
-    precision = np.zeros(6)
-    recall = np.zeros(6)
-    for j in range(6):
-        if tp[j] + fp[j] > 0:
-            precision[j] = tp[j] / (tp[j] + fp[j])
-        if tp[j] + fn[j] > 0:
-            recall[j] = tp[j] / (tp[j] + fn[j])
-
-    # Print the results
-    print(precision[0],recall[0],precision[1],recall[1],precision[2],recall[2],precision[3],recall[3],precision[4],recall[4],precision[5],recall[5])
-
-
-# In[10]:
-
+        hits,category=track_injection(hits,pos_events_val,neg_events_val)    
+    return hits.astype(bool), category.astype(int)
 
 learning_rate_filter=1e-6
 callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
 n_train = 0
 
-
-# In[11]:
-
 print("Before while loop:", n_train)
 while(n_train<1e7):
-    trainin, trainsignals, traintrack = generate_e906(500000, "Train")
+    trainin, trainsignals = generate_e906(500000, "Train")
     n_train+=len(trainin)
-    del traintrack
     print("Generated Training Data")
-    valin, valsignals, valtrack = generate_e906(50000, "Val")
-    del valtrack
+    valin, valsignals = generate_e906(50000, "Val")
     print("Generated Validation Data")
     tf.keras.backend.clear_session()
     tf.compat.v1.reset_default_graph()
     gc.collect()
-    model = keras.models.load_model('Networks/event_filter')
+    model = tf.keras.models.load_model('Networks/event_filter')
     optimizer = tf.keras.optimizers.Adam(learning_rate_filter)
     model.compile(optimizer=optimizer,
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
@@ -631,7 +406,6 @@ while(n_train<1e7):
         model.save('Networks/event_filter')
         learning_rate_filter *=2
     learning_rate_filter /=2
-    evaluate(valin,valsignals,"event_filter")
     print('\n')
     tf.keras.backend.clear_session()
     del trainsignals,trainin,valin,valsignals,model
