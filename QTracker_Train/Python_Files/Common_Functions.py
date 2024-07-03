da5ac5288694dcb538ec889078dbc3487db9198b
AConover <50592596+AConover@users.noreply.github.com>
2024-07-03 10:06:26 -0400

    Update Common_Functions.py

--- QTracker_Train/Python_Files/Common_Functions.py
+++ QTracker_Train/Python_Files/Common_Functions.py
@@ -6,7 +6,7 @@ def read_root_file(root_file):
     targettree = uproot.open(root_file + ':QA_ana')
     targetevents = len(targettree['n_tracks'].array(library='np'))
 
-    detector_data = read_detector_data(targettree)
+    mc_data = read_mc_data(targettree)
 
     pos_events = np.zeros((targetevents, 54))
     pos_drift = np.zeros((targetevents, 30))
@@ -15,38 +15,42 @@ def read_root_file(root_file):
     neg_drift = np.zeros((targetevents, 30))
     neg_kinematics = np.zeros((targetevents, 6))
     
-    process_target_events(targetevents, detector_data, pos_events, pos_drift, pos_kinematics, neg_events, neg_drift, neg_kinematics)
+    process_target_events(targetevents, mc_data, pos_events, pos_drift, pos_kinematics, neg_events, neg_drift, neg_kinematics)
     
     return pos_events, pos_drift, pos_kinematics, neg_events, neg_drift, neg_kinematics
 
-def read_detector_data(targettree):
-    detector_data = {}
-    for key in ['D0U_ele', 'D0Up_ele', 'D0X_ele', 'D0Xp_ele', 'D0V_ele', 'D0Vp_ele', 
+def read_mc_data(targettree):
+    mc_data = {}
+    for keys in ['D0U_ele', 'D0Up_ele', 'D0X_ele', 'D0Xp_ele', 'D0V_ele', 'D0Vp_ele', 
                 'D2U_ele', 'D2Up_ele', 'D2X_ele', 'D2Xp_ele', 'D2V_ele', 'D2Vp_ele', 
                 'D3pU_ele', 'D3pUp_ele', 'D3pX_ele', 'D3pXp_ele', 'D3pV_ele', 'D3pVp_ele', 
                 'D3mU_ele', 'D3mUp_ele', 'D3mX_ele', 'D3mXp_ele', 'D3mV_ele', 'D3mVp_ele', 
                 'H1B_ele', 'H1T_ele', 'H1L_ele', 'H1R_ele', 'H2L_ele', 'H2R_ele', 'H2B_ele', 'H2T_ele', 
                 'H3B_ele', 'H3T_ele', 'H4Y1L_ele', 'H4Y1R_ele', 'H4Y2L_ele', 'H4Y2R_ele', 'H4B_ele', 'H4T_ele', 
                 'P1Y1_ele', 'P1Y2_ele', 'P1X1_ele', 'P1X2_ele', 'P2X1_ele', 'P2X2_ele', 'P2Y1_ele', 'P2Y2_ele', 
+                'D0U_drift', 'D0Up_drift', 'D0X_drift', 'D0Xp_drift', 'D0V_drift', 'D0Vp_drift', 
+                'D2U_drift', 'D2Up_drift', 'D2X_drift', 'D2Xp_drift', 'D2V_drift', 'D2Vp_drift', 
+                'D3pU_drift', 'D3pUp_drift', 'D3pX_drift', 'D3pXp_drift', 'D3pV_drift', 'D3pVp_drift', 
+                'D3mU_drift', 'D3mUp_drift', 'D3mX_drift', 'D3mXp_drift', 'D3mV_drift', 'D3mVp_drift',
                 'gpx', 'gpy', 'gpz', 'gvx', 'gvy', 'gvz', 'pid']:
-        detector_data[key] = targettree[key].array(library='np')
-    return detector_data
+        mc_data[keys] = targettree[keys].array(library='np')
+    return mc_data
 
-def process_target_events(targetevents, detector_data, pos_events, pos_drift, pos_kinematics, neg_events, neg_drift, neg_kinematics):
-    pid = detector_data['pid']
-    gpx = detector_data['gpx']
-    gpy = detector_data['gpy']
-    gpz = detector_data['gpz']
-    gvx = detector_data['gvx']
-    gvy = detector_data['gvy']
-    gvz = detector_data['gvz']
+def process_target_events(targetevents, mc_data, pos_events, pos_drift, pos_kinematics, neg_events, neg_drift, neg_kinematics):
+    pid = mc_data['pid']
+    gpx = mc_data['gpx']
+    gpy = mc_data['gpy']
+    gpz = mc_data['gpz']
+    gvx = mc_data['gvx']
+    gvy = mc_data['gvy']
+    gvz = mc_data['gvz']
     
     for j in range(targetevents):
         first = pid[j][0]
         pos, neg = (0, 1) if first > 0 else (1, 0)
         
         fill_kinematics(gpx, gpy, gpz, gvx, gvy, gvz, pos_kinematics, neg_kinematics, j, pos, neg)
-        fill_events_and_drift(detector_data, pos_events, pos_drift, neg_events, neg_drift, j, pos, neg)
+        fill_events_and_drift(mc_data, pos_events, pos_drift, neg_events, neg_drift, j, pos, neg)
 
 def fill_kinematics(gpx, gpy, gpz, gvx, gvy, gvz, pos_kinematics, neg_kinematics, j, pos, neg):
     pos_kinematics[j][0] = gpx[j][pos]
@@ -62,13 +66,24 @@ def fill_kinematics(gpx, gpy, gpz, gvx, gvy, gvz, pos_kinematics, neg_kinematics
     neg_kinematics[j][4] = gvy[j][neg]
     neg_kinematics[j][5] = gvz[j][neg]
 
-def fill_events_and_drift(detector_data, pos_events, pos_drift, neg_events, neg_drift, j, pos, neg):
+def fill_events_and_drift(mc_data, pos_events, pos_drift, neg_events, neg_drift, j, pos, neg):
+    keys = ['D0U_ele', 'D0Up_ele', 'D0X_ele', 'D0Xp_ele', 'D0V_ele', 'D0Vp_ele', 
+                'D2U_ele', 'D2Up_ele', 'D2X_ele', 'D2Xp_ele', 'D2V_ele', 'D2Vp_ele', 
+                'D3pU_ele', 'D3pUp_ele', 'D3pX_ele', 'D3pXp_ele', 'D3pV_ele', 'D3pVp_ele', 
+                'D3mU_ele', 'D3mUp_ele', 'D3mX_ele', 'D3mXp_ele', 'D3mV_ele', 'D3mVp_ele', 
+                'H1B_ele', 'H1T_ele', 'H1L_ele', 'H1R_ele', 'H2L_ele', 'H2R_ele', 'H2B_ele', 'H2T_ele', 
+                'H3B_ele', 'H3T_ele', 'H4Y1L_ele', 'H4Y1R_ele', 'H4Y2L_ele', 'H4Y2R_ele', 'H4B_ele', 'H4T_ele', 
+                'P1Y1_ele', 'P1Y2_ele', 'P1X1_ele', 'P1X2_ele', 'P2X1_ele', 'P2X2_ele', 'P2Y1_ele', 'P2Y2_ele', 
+                'D0U_drift', 'D0Up_drift', 'D0X_drift', 'D0Xp_drift', 'D0V_drift', 'D0Vp_drift', 
+                'D2U_drift', 'D2Up_drift', 'D2X_drift', 'D2Xp_drift', 'D2V_drift', 'D2Vp_drift', 
+                'D3pU_drift', 'D3pUp_drift', 'D3pX_drift', 'D3pXp_drift', 'D3pV_drift', 'D3pVp_drift', 
+                'D3mU_drift', 'D3mUp_drift', 'D3mX_drift', 'D3mXp_drift', 'D3mV_drift', 'D3mVp_drift']
     for k in range(54):
-        pos_events[j][k] = detector_data[f'key_{k}_ele'][j][pos]
-        neg_events[j][k] = detector_data[f'key_{k}_ele'][j][neg]
+        pos_events[j][k] = mc_data[keys[k]][j][pos]
+        neg_events[j][k] = mc_data[keys[k]][j][neg]
     for k in range(30):
-        pos_drift[j][k] = detector_data[f'key_{k}_drift'][j][pos]
-        neg_drift[j][k] = detector_data[f'key_{k}_drift'][j][neg]
+        pos_drift[j][k] = mc_data[keys[k+54]][j][pos]
+        neg_drift[j][k] = mc_data[keys[k+54]][j][neg]
 
 @njit()
 def hit_matrix(detectorid,elementid,drifttime,hits,drift,station): #Convert into hit matrices
@@ -102,7 +117,7 @@ def build_background(n_events):
     filelist=['output_part1.root:tree_nim3','output_part2.root:tree_nim3','output_part3.root:tree_nim3',
              'output_part4.root:tree_nim3','output_part5.root:tree_nim3','output_part6.root:tree_nim3',
              'output_part7.root:tree_nim3','output_part8.root:tree_nim3','output_part9.root:tree_nim3']
-    targettree = uproot.open("NIM3/"+random.choice(filelist))
+    targettree = uproot.open("/project/ptgroup/QTracker_Training/NIM3/"+random.choice(filelist))
     detectorid_nim3=targettree["det_id"].arrays(library="np")["det_id"]
     elementid_nim3=targettree["ele_id"].arrays(library="np")["ele_id"]
     driftdistance_nim3=targettree["drift_dist"].arrays(library="np")["drift_dist"]
